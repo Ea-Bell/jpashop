8f6b82ef5e27e8090ccbe1b7e51deac91fb696f4
Ea-Bell <cjp975@naver.com>
2021-08-11 01:00:23 +0900

    스프링 시작, 스프링컨테이너 생성, 컨테이너에 등록된 모든 빈 조회, 다음회 스프링 빈을 조회-기본 듣기

--- src/main/java/hello/core/oderApp/OderApp.java
+++ src/main/java/hello/core/oderApp/OderApp.java
@@ -1,24 +1,28 @@
 package hello.core.oderApp;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import hello.core.AppConfig;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
 import hello.core.order.Order;
+import hello.core.order.OrderService;
 
 public class OderApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService oderService = appConfig.orderService();
+        // AppConfig appConfig = new AppConfig();
+        // MemberService memberService = appConfig.memberService();
+        // OrderService oderService = appConfig.orderService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
-        Order oder = oderService.createOrder(memberId, "itemA", 20000);
+        Order oder = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order = " + oder);
         System.out.println("order.calculatePrice = " + oder.calculatePrice());
     }
