8c455e1bd6653bd8dfe57ec2bb0f7cfbd7e78cbb
Ea-Bell <cjp975@naver.com>
2021-08-29 18:10:26 +0900

    • 스프링 컨테이너에 요청할 때 마다 새로 생성된다. • 스프링 컨테이너는 프로토타입 빈의 생성과 의존관계 주입 그리고 초기화까지만 관여한다. •  종료 메서드가 호출되지 않는다. 프로토타입 빈은 프로토타입 빈을 조회한 클라이언트가 관리해야 한다. 종료 메서드에 대한 호출도 클라이언트가 직접 해야한다.

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonTest {
+
+    @Test
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletoneBean.class);
+        SingletoneBean singletoneBean1 = ac.getBean(SingletoneBean.class);
+        SingletoneBean singletoneBean2 = ac.getBean(SingletoneBean.class);
+        System.out.println("singletonBean1 = " + singletoneBean1);
+        System.out.println("singletonBean2 = " + singletoneBean2);
+        Assertions.assertThat(singletoneBean1).isSameAs(singletoneBean2);
+        ac.close();
+    }
+
+    @Scope("singleton")
+    static class SingletoneBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destory() {
+            System.out.println("SingletonBean.destory");
+        }
+    }
+}
